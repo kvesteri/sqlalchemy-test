b1a7294b383ac54ad5d5dc25fe10c1c0a3ee5c18
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-09-04 12:19:40 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,51 @@
+"""
+sqlalchemy-test
+-------------------
+
+Various unit test helpers for SQLAlchemy models.
+"""
+
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+setup(
+    name='sqlalchemy-test',
+    version='0.1.0',
+    url='https://github.com/kvesteri/sqlalchemy-test',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description='Various unit test helpers for SQLAlchemy models.',
+    long_description=__doc__,
+    packages=['sqlalchemy_test'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'SQLAlchemy>=0.7',
+    ],
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
